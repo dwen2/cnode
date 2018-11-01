8d7dffc62987d15d3ccb6a515d71f2f0dc76865b
Dezhang Wen <dezhang12@gmail.com>
2018-11-01 19:34:00 +0000

    first

--- routes/index.js
+++ routes/index.js
@@ -5,43 +5,15 @@ var request = require("request");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile('weather.html', { root: 'public' });
+    res.sendFile('index.html', { root: 'public' });
 });
 
-router.get('/getcity', function(req, res, next) {
-
-    fs.readFile(__dirname + '/cities.dat.txt', function(err, data) {
-        if (err) throw err;
-        var cities = data.toString().split("\n");
-        for (var i = 0; i < cities.length; i++) {
-            var result = cities[i].search(myRe);
-            if (result != -1) {
-                console.log(cities[i]);
-            }
-        }
-
-        var jsonresult = [];
-        for (var i = 0; i < cities.length; i++) {
-            var result = cities[i].search(myRe);
-            if (result != -1) {
-                console.log(cities[i]);
-                jsonresult.push({ city: cities[i] });
-            }
-        }
-        console.log(jsonresult);
-        res.status(200).json(jsonresult);
-    });
-
-    var myRe = new RegExp("^" + req.query.q);
-
-});
-
-router.get('/dictionary', function(req, res, next) {
-    var url = "https://owlbot.info/api/v1/dictionary/"
-    url += req.query['q'];
-    url += "?format=json";
+router.get('/joke', function(req, res, next) {
+    var url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com"
+    url += "/dev/random_joke"; 
     console.log(url);
     request(url).pipe(res);
+   
 });
 
 module.exports = router;
